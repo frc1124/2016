f17250bf4290e7d5a9be72600222a046366f10bb
CurrenJ <currenjeandell@me.com>
2016-01-24 14:29:11 -0500

    Rewrote parts of ConfigIO
    configIO sucks

--- UberCode2016/src/org/usfirst/frc/team1124/robot/tools/ConfigIO.java
+++ UberCode2016/src/org/usfirst/frc/team1124/robot/tools/ConfigIO.java
@@ -20,54 +20,59 @@ public class ConfigIO {
 	static Map<String, String> config;
 	static BufferedReader br1;
 	static ArrayList<String> rawConfig;
-	
+	static ArrayList<String> fullConfig;
+	//I HATE FISHLER
+
 	public ConfigIO(){
 		rawConfig = new ArrayList<String>();
+		fullConfig = new ArrayList<String>();
 		file = new File(filePath);
 		config = new HashMap<String, String>();
-		
+
 		try{
 			file.createNewFile();
 		}catch(IOException e1){
 			e1.printStackTrace();
 		}
-		
+
 		reloadConfig();
 		//		for(int v = 0; v < config.size(); v++)
 		//			System.out.println(config.keySet().toArray()[v] + " = " + config.get(config.keySet().toArray()[v]));
 	}
-	
+
 	/** Get the string value from the config file.*/
 	public String getStringVal(String key){
 		return config.get(key);
 	}
-	
+
 	/** Get the integer value from the config file.*/
 	public int getIntVal(String key){
 		return Integer.parseInt(config.get(key));
 	}
-	
+
 	private void reloadConfig(){
 		try{ 
 			br1 = new BufferedReader(new FileReader(filePath)); 
 		}catch(FileNotFoundException e){
 			e.printStackTrace();
 		}
-		
+
 		config.clear();
 		rawConfig.clear();
+		fullConfig.clear();
 
 		try{
 			String line = br1.readLine();
-			
+
 			while(line != null){
+				fullConfig.add(line);
 				if(line.charAt(0) != '#'){
 					String value1 = "";
 					String value2 = "";
 					rawConfig.add(line);
-					
+
 					boolean encounteredSpace = false;
-				
+
 					for(int l = 0; l < line.length() && !encounteredSpace; l++){
 						if(line.charAt(l) != ' '){
 							value1 += line.charAt(l);
@@ -76,12 +81,12 @@ public class ConfigIO {
 							encounteredSpace = true;
 						}
 					}
-					
+
 					if(encounteredSpace){
 						config.put(value1, value2);
 					}
 				}
-				
+
 				line = br1.readLine();
 			}
 		}catch(IOException e){ 
@@ -94,24 +99,20 @@ public class ConfigIO {
 			} 
 		}
 	}
-	
+
 	/** */
 	public ArrayList<String> getConfigText(){
 		reloadConfig();
 		return rawConfig;
 	}
-	
+
 	/** Writes keys and values to config map. If the key already exists it overrides the existing value. */
-	public void writeKeyVal(String key, String value){
+	public void changeKeyVal(String key, String value){
 		reloadConfig();
 		config.put(key, value);
-		writeConfigToFile();
-	}
-	
-	/** Pushes the in-code config map to config file */
-	private void writeConfigToFile(){
+
 		PrintWriter wr0 = null;
-		
+
 		try{
 			wr0 = new PrintWriter(filePath, "UTF-8");
 		}catch(FileNotFoundException e){
@@ -119,14 +120,36 @@ public class ConfigIO {
 		}catch(UnsupportedEncodingException e){
 			e.printStackTrace();
 		}
-	
+
+		for(int c = 0; c < fullConfig.size(); c++)
+			for(int i = 0; i <  config.size(); i++){
+				String tempKey = (String) config.keySet().toArray()[i];
+				String tempVal = config.get(tempKey);
+				
+				String fCLine = fullConfig.get(c);
+				String fCKey = "";
+				for(int s = 0; s < fCLine.length() && fCLine.charAt(0) != '#' && fCLine.charAt(s) != ' '; s++){
+					if(fCLine.charAt(s) != ' ')
+						fCKey += fCLine.charAt(s);
+				}
+				
+				if(fCKey.equals(tempKey)){
+					wr0.println(tempKey + " " + tempVal);
+				}
+				else wr0.println(fCLine);
+			}
+
+		wr0.close();
+	}
+
+	/** Pushes the in-code config map to config file */
+	private void writeConfigToFile(){
+
+
 		for(int i = 0; i <  config.size(); i++){
 			String tempKey = (String) config.keySet().toArray()[i];
 			String tempVal = config.get(tempKey);
-			
-			wr0.println(tempKey + " " + tempVal);
 		}
-		
 		wr0.close();
 	}
 }
