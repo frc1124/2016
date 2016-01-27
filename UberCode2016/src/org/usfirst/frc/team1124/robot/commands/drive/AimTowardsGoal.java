fd8f88c62faff76370c574b707cc646ce7e1c72c
SlugKing <pfnpfnpfn@gmail.com>
2016-01-26 20:18:06 -0500

    crude aim drive train code
    still need to set constant(s) and this is NOT the shooter speed value,
    it gets an angle from the vision shit

---
+++ UberCode2016/src/org/usfirst/frc/team1124/robot/commands/drive/AimTowardsGoal.java
@@ -0,0 +1,52 @@
+package org.usfirst.frc.team1124.robot.commands.drive;
+
+import org.usfirst.frc.team1124.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+
+
+/** 
+ * I sexually identify as an Attack Helicopter. Ever since I was a boy I dreamed of soaring over the oil fields, dropping packets on Fischler like they're hot, cause Snoop said to.
+ */
+public class AimTowardsGoal extends Command {
+
+	private double angleFromGoal = 0; // in radians
+	private double distanceToTurn = 0;
+	private AutoDrive turnCommand;
+	private final double ROBOT_LENGTH = 0; // for distance purposes, set this plssss
+	
+    public AimTowardsGoal(double angle) // WHOEVER SEES THIS FOR REFERENCE, MAKE NEGATIVE ANGLES TO THE RIGHT AND POSITIVE TO THE LEFT (e.g. -.785 would mean 90 degrees to the right of the goal, but that better not happen)
+    {
+    	// take angle
+    	// do wizard to find what distance to turn (PID)
+    	// turn based on that.
+    	
+    	angleFromGoal = angle;
+    	requires(Robot.drivetrain);
+    }
+
+    protected void initialize()
+    {
+    	distanceToTurn = (angleFromGoal/2)*Math.pow(ROBOT_LENGTH/2, 2); // if the math is different feel free to change lol
+    	if (angleFromGoal < 0)
+    	{
+    		turnCommand = new AutoDrive(distanceToTurn, 0);
+    	}
+    	else if (angleFromGoal > 0)
+    	{
+    		turnCommand = new AutoDrive(0, distanceToTurn);
+    	}
+		Scheduler.getInstance().add(turnCommand);
+    }
+
+    protected void execute() {}
+
+    protected boolean isFinished() {
+    		return !turnCommand.isRunning();
+    }
+
+    protected void end() {}
+
+    protected void interrupted() {}
+}
