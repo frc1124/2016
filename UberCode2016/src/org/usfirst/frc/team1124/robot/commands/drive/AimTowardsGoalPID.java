dddfebbd83a143f12d9c95e8f385881e78ca9e8e
Michael <micron1314@gmail.com>
2016-02-03 21:27:07 -0500

    tuning of vision aiming

--- UberCode2016/src/org/usfirst/frc/team1124/robot/commands/drive/AimTowardsGoalPID.java
+++ UberCode2016/src/org/usfirst/frc/team1124/robot/commands/drive/AimTowardsGoalPID.java
@@ -3,14 +3,14 @@ package org.usfirst.frc.team1124.robot.commands.drive;
 import org.usfirst.frc.team1124.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.PIDCommand;
-import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.command.Scheduler;
 
 /**
  *
  */
 public class AimTowardsGoalPID extends PIDCommand {
-	private static final double P = 0.00001;
-	private static final double I = 0;
+	private static final double P = 0.0015;
+	private static final double I = 0.000042;
 	private static final double D = 0;
 	
 	private static final int cam_width = 320;
@@ -26,13 +26,7 @@ public class AimTowardsGoalPID extends PIDCommand {
 	protected double returnPIDInput() {
 		double center = Robot.db_connection.getTargetCenterOfMass()[0];
 		
-		if(center == -1){
-			isDone = true;
-			
-			return this.getSetpoint();
-		}else{ 
-			return Robot.db_connection.getTargetCenterOfMass()[0];
-		}
+		return center;
 	}
 
 	protected void usePIDOutput(double output) {
@@ -41,8 +35,17 @@ public class AimTowardsGoalPID extends PIDCommand {
 
 	protected void end() {}
 
-	protected void execute() {}
-
+	protected void execute() {
+		double center = Robot.db_connection.getTargetCenterOfMass()[0];
+		
+		if(center == 0){
+			getPIDController().disable();
+			getPIDController().reset();
+		}else{
+			getPIDController().enable();
+		}
+	}
+	
 	protected void initialize() {
 		setSetpoint(cam_width / 2);
 	}
