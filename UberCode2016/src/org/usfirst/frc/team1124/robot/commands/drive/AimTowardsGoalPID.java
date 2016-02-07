3c5c520f85e7959428decf591540534a7e668ada
Michael <micron1314@gmail.com>
2016-02-07 10:47:36 -0500

    tuning

--- UberCode2016/src/org/usfirst/frc/team1124/robot/commands/drive/AimTowardsGoalPID.java
+++ UberCode2016/src/org/usfirst/frc/team1124/robot/commands/drive/AimTowardsGoalPID.java
@@ -6,12 +6,12 @@ import edu.wpi.first.wpilibj.command.PIDCommand;
 import edu.wpi.first.wpilibj.command.Scheduler;
 
 /**
- *
+ * 
  */
 public class AimTowardsGoalPID extends PIDCommand {
-	private static final double P = 0.0013999;
-	private static final double I = 0.0002578;
-	private static final double D = 0.0048184;
+	private static final double P = 0.0023666;
+	private static final double I = 0.00021213;
+	private static final double D = 0.002804366760;
 	
 	private static final int cam_width = 320;
 	private static boolean isDone = false;
@@ -22,19 +22,19 @@ public class AimTowardsGoalPID extends PIDCommand {
     	requires(Robot.drivetrain);
     	setInterruptible(true);
     }
-
+    
 	protected double returnPIDInput() {
 		double center = Robot.db_connection.getTargetCenterOfMass()[0];
 		
 		return center;
 	}
-
+	
 	protected void usePIDOutput(double output) {
 		Robot.drivetrain.drive_tank_auto((-1) * output, output);
 	}
-
+	
 	protected void end() {}
-
+	
 	protected void execute() {
 		double center = Robot.db_connection.getTargetCenterOfMass()[0];
 		
