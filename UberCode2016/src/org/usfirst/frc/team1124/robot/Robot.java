26db7e94e128c0e6c6be2f2584854abd797defd2
Michael <micron1314@gmail.com>
2016-03-03 18:55:15 -0500

    break during disabled

--- UberCode2016/src/org/usfirst/frc/team1124/robot/Robot.java
+++ UberCode2016/src/org/usfirst/frc/team1124/robot/Robot.java
@@ -146,7 +146,9 @@ public class Robot extends IterativeRobot {
      * This function is called when the disabled button is hit.
      * You can use it to reset subsystems before shutting down.
      */
-    public void disabledInit(){}
+    public void disabledInit(){
+    	drivetrain.setBrake();
+    }
 
     /**
      * This function is called periodically during operator control
