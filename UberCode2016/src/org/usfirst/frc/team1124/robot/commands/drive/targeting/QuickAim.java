ea62bd058bdb905d8bad508611443030981a5f88
Michael <micron1314@gmail.com>
2016-03-09 16:50:45 -0500

    fast targeting; gotta go fast

---
+++ UberCode2016/src/org/usfirst/frc/team1124/robot/commands/drive/targeting/QuickAim.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team1124.robot.commands.drive.targeting;
+
+import org.usfirst.frc.team1124.robot.Robot;
+import org.usfirst.frc.team1124.robot.tools.Aim;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * Gotta go fast.
+ */
+public class QuickAim extends Command {
+	
+	private double x;
+	private double speed = 0;
+	
+	private Aim aim;
+	
+    public QuickAim() {
+        requires(Robot.drivetrain);
+        
+        x = Robot.camera.getTargetCenterOfMass()[0];
+    }
+
+    protected void initialize() {
+    	aim = new Aim(x);
+    }
+
+    protected void execute() {
+    	speed = aim.getOutput();
+    	Robot.drivetrain.drive_tank_auto((-1) * speed, speed);
+    }
+
+    protected boolean isFinished() {
+        return aim.done();
+    }
+
+    protected void end() {
+    	Robot.drivetrain.stop();
+    }
+
+    protected void interrupted() {
+    	end();
+    }
+}
--- UberCode2016/src/org/usfirst/frc/team1124/robot/tools/Aim.java
+++ UberCode2016/src/org/usfirst/frc/team1124/robot/tools/Aim.java
@@ -1,21 +1,29 @@
 package org.usfirst.frc.team1124.robot.tools;
 
+import java.util.Arrays;
+
 public class Aim {
 	private double[] outputs = new double[10];
 	
 	private int tick = 0;
+	private int num_steps = 10;
 	private int tick_time = 5; // run 10 times in a second
 	
 	private double stop_voltage = 0.14;
 	
 	public static void main(String args[]){
-		System.out.println("Calculating speed for 15 pixels away...");
+		System.out.print("Calculating speed for ");
 		
-		Aim aim = new Aim(15);
+		Aim aim = new Aim(120);
 		
+		System.out.println(" pixels away...");
+		
+		/*
 		for(int i = 0; i < 50; i++){
 			System.out.println(aim.getOutput());
-		}
+		}*/
+		
+		System.out.println(Arrays.toString(aim.outputs));
 	}
 	
 	/**
@@ -26,7 +34,9 @@ public class Aim {
 		// negative is to the left, positive is to the right
 		double pos = -1 * (pixel_pos - 160);
 		
-		for(int i = 0; i < 10; i++){
+		System.out.print(pos);
+		
+		for(int i = 0; i < num_steps; i++){
 			outputs[i] = calculate(pos, i);
 		}
 	}
@@ -38,7 +48,11 @@ public class Aim {
 		return speed;
 	}
 	
-	public double calculate(double x, int tick){
+	public boolean done(){
+		return tick_time * num_steps >= tick;
+	}
+	
+	private double calculate(double x, int tick){
 		double speed = 0;
 		double sign = 0;
 		
@@ -53,7 +67,7 @@ public class Aim {
 			}else if(x > 2){
 				speed = -0.0007867 * Math.pow((sign * x), 4) + 0.020648 * Math.pow((sign * x), 3) + -0.1848 * Math.pow((sign * x), 2) + 0.6149 * (sign * x) + -0.05; 
 			}else{
-				speed = stop_voltage;
+				speed = sign * stop_voltage;
 			}
 		}else if(tick <= 4){
 			if(x > 80){
@@ -63,13 +77,14 @@ public class Aim {
 			}else if(x > 2){
 				speed = -0.0007867 * Math.pow((sign * x), 4) + 0.020648 * Math.pow((sign * x), 3) + -0.1848 * Math.pow((sign * x), 2) + 0.6149 * (sign * x) + -0.05;
 			}else{
-				speed = stop_voltage;
+				speed = sign * stop_voltage;
 			}
-			
+
+			// decay transform
 			if(x > 2){
-				speed -= sign * (0.4);
+				speed -= (0.4);
 			}
-		}else if(tick <= 9){
+		}else if(tick <= 8){
 			if(x > 80){
 				speed = 0.8 * sign;
 			}else if(x > 10){
@@ -77,13 +92,19 @@ public class Aim {
 			}else if(x > 2){
 				speed = -0.0007867 * Math.pow((sign * x), 4) + 0.020648 * Math.pow((sign * x), 3) + -0.1848 * Math.pow((sign * x), 2) + 0.6149 * (sign * x) + -0.05;
 			}else{
-				speed = stop_voltage;
+				speed = sign * stop_voltage;
 			}
 			
-			if(x > 2){
-				speed -= sign * (0.4);
-				speed /= -0.1 * (tick - 5);
+			// decay and reverse transform
+			if(x > 10){
+				speed += (0.05);
+				speed *= -0.1 * (9 - tick);
+			}else if(x > 2){
+				speed -= (0.4);
+				speed *= -0.1 * (9 - tick);
 			}
+		}else{
+			speed = sign * stop_voltage;
 		}
 		
 		return speed;
