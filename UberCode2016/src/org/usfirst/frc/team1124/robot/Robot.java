17520bf34bd372944d80266282a9323f4040c12e
Michael1516 <micron1314@gmail.com>
2016-04-02 16:13:58 -0400

    fixed ramparts not fully crossing
    it could shoot, just didn't count as a crossing

--- UberCode2016/src/org/usfirst/frc/team1124/robot/Robot.java
+++ UberCode2016/src/org/usfirst/frc/team1124/robot/Robot.java
@@ -73,7 +73,7 @@ public class Robot extends IterativeRobot {
     
     // this revision of code (displayed on dash board)
     
-    public static String codeRevision = "[v5.1.4]:week_5:auto";
+    public static String codeRevision = "[v5.1.6]:week_5:ramparts";
 
     /**
      * This function is run when the robot is first started up and should be
