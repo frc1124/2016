fbb801030b5f06b167320a853cd6057c5c7fc155
Michael1516 <micron1314@gmail.com>
2016-04-06 22:40:24 -0400

    cleanup

--- UberCode2016/src/org/usfirst/frc/team1124/robot/CurrenException.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team1124.robot;
-
-@SuppressWarnings("serial")
-public class CurrenException extends RuntimeException {
-	public CurrenException() { super(); }
-	public CurrenException(String message) { super(message); };
-	public CurrenException(String message, Throwable cause) { super(message, cause); }
-	public CurrenException(Throwable cause) { super(cause); }	
-}
