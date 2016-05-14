7a53de6a5fe95346baa8733540e1ec2a6375010b
Michael1516 <micron1314@gmail.com>
2016-05-14 12:15:25 -0400

    [day 1] shooter speed and ramparts auto

--- UberCode2016/src/org/usfirst/frc/team1124/robot/commands/macro/Autonomous.java
+++ UberCode2016/src/org/usfirst/frc/team1124/robot/commands/macro/Autonomous.java
@@ -96,12 +96,12 @@ public class Autonomous extends CommandGroup {
 			break;
 			case Ramparts:
 				//addSequential(new TimedAutoDrive(0.8, 0.8, 0.5));
-				addSequential(new TimedAutoDrive(0.8, 0.8, 0.5));
+				addSequential(new TimedAutoDrive(0.8, 0.8, 0.55));
 				//addSequential(new TimedAutoDrive(0.55, 1.0, 0.55));
 				addSequential(new TimedAutoDrive(0.55, 1.0, 0.55));
 				//addSequential(new TimedAutoDrive(0.9, 0.9, 0.9));
 				//addSequential(new TimedAutoDrive(0.8, 0.9, 1.15));
-				addSequential(new TimedAutoDrive(0.9, 0.9, 1.0));
+				addSequential(new TimedAutoDrive(0.9, 0.9, 1.50));
 				addSequential(new CommandDelay(0.4));
 			break;
 			case RockWall:
